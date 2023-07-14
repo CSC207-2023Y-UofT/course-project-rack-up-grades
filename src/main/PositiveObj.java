eae005b97e1cf18da51c899f843d7b29d2adcc1d
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2023-07-13 20:03:14 -0400

    Add class

---
+++ src/main/PositiveObj.java
@@ -0,0 +1,10 @@
+public class PositiveObj {
+    private float point;
+
+    public PositiveObj(int point) {
+        this.point = point;
+    }
+    public float getPoint() {
+        return this.point;
+    }
+}
