9907e940be6b9c187271c1dc3796a9dae1030f89
Shreya Sakura Noskor <sakura.noskor12@gmail.com>
2023-07-16 14:16:10 -0400

    changed the attributes of the entity class

--- src/main/java/NegativeObj.java
+++ src/main/java/NegativeObj.java
@@ -1,13 +1,11 @@
 public class NegativeObj {
     // This is the class that creates the negative objects and its getter and setters
-    private double PRIVATE = 1.2;
+
     private float point;
 
-    private String name;
 
     public NegativeObj(String name, int point){
         //the point has to be negative
-        this.name = name;
         if(Math.signum(point) >= 0){
             this.point = - point;
         }
@@ -20,15 +18,9 @@ public class NegativeObj {
     public float getPoint(){
         return this.point;
     }
-
-    public String getName(){
-        return this.name;
-    }
 }
 
 abstract class PositiveObj{
-    private double PRIVATE = 1.2;
     private float point;
 
-    private String name;
 }
