0114577fdeecb64102fec943c765c03fb27902f7
Baehr3 <andy.huang12345@outlook.com>
2023-07-23 14:18:46 -0400

    Updated all controllers
    Documented some method/constructors and started to work on game controller methods. And added it to the central controller

--- src/main/java/LeaderboardController.java
+++ src/main/java/LeaderboardController.java
@@ -8,7 +8,10 @@ public class LeaderboardController {
     private InputBoundary input;
     private String name;
 
-
+    /**
+     * constructs a Leaderboard Controller and stores the InputBoundary for use
+     * @param input
+     */
     public LeaderboardController(InputBoundary input) {
         this.input = input;
     }
@@ -16,11 +19,11 @@ public class LeaderboardController {
 
     /**
      * This method is used to set the name that's going to be added to the
-     * leaderboard through the input boundary and use case.
+     * leaderboard through the InputBoundary to use case.
      * @param newName
      */
     public void setName(String newName) {
-        input.name = newName;
+        input.setName(newName);
     }
 
 }
--- src/main/java/NavigatorController.java
+++ src/main/java/NavigatorController.java
@@ -1,14 +1,36 @@
 public class NavigatorController {
     private InputBoundary input;
     private LeaderboardController LC;
+    private GameController GC;
+
+    /**
+     * constructs a Navigator Controller as the central controller for Leaderboard Controller
+     * and Game Controller by creating an instance of them and saving them here
+     * stores the instance of InputBoundary
+     * @param input
+     */
     public NavigatorController(InputBoundary input) {
         this.input = input;
         LC = new LeaderboardController(input);
+        GC = new GameController(input);
 
     }
 
+    /**
+     * Refer to Leaderboard Controller documentation
+     * @param newName
+     */
     public void setName(String newName) {
         LC.setName(newName);
+    }
+
+
+    /**
+     * Refer to Game Controller documentation
+     * @param i
+     */
+    public void click(Integer i) {
+        GC.click(i);
     }
 
 }
