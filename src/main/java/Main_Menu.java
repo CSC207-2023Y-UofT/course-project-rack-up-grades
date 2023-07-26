7dd8c8fbbec6a4412f3c4a51e8468f785152604e
Cathynvrdie <xinyi.yan@mail.utoronto.ca>
2023-07-25 20:37:46 -0400

    Main Menu 1.2 *Connect the buttons with the button listener

--- src/main/java/Main_Menu.java
+++ src/main/java/Main_Menu.java
@@ -1,20 +1,20 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main_Menu extends JPanel {
+public class Main_Menu extends JFrame {
+    JButton i,easy,medium,hard,leaderboard;
 
-    public static void main(String[] args) {
-        JFrame mainmenu = new JFrame("Rack Up Grade - Main Menu");
-        mainmenu.setSize(1200,700);
-        mainmenu.setLocationRelativeTo(null);
-        JButton i = new JButton("I"); //Icon will be done
-        JButton easy = new JButton("Easy");
-        JButton medium = new JButton("Medium");
-        JButton hard = new JButton("Hard");
-        JButton leaderboard = new JButton("Leaderboard");//buttons
-        //
-        JPanel menu = new JPanel();
-        menu.setLayout(null);
+    public Main_Menu(String title) {
+        super(title);
+        this.i = new JButton("I"); //Icon will be done
+        this.easy = new JButton("Easy");
+        this.medium = new JButton("Medium");
+        this.hard = new JButton("Hard");
+        this.leaderboard = new JButton("Leaderboard");//buttons
+    }
+
+    public void setAndAdd(JPanel menu) {
         menu.add(i);
         i.setBounds(1000,100,100,100);
         menu.add(easy);
@@ -25,11 +25,48 @@ public class Main_Menu extends JPanel {
         hard.setBounds(800,350,200,100);
         menu.add(leaderboard);
         leaderboard.setBounds(400,500,400,80);
+    }
+
+    public static void main(String[] args) {
+        Main_Menu mainmenu = new Main_Menu("Rack Up Grade - Main Menu");
+        mainmenu.setSize(1200,700);
+        mainmenu.setLocationRelativeTo(null);
+        mainmenu.setUpButtonListeners();
+        JPanel menu = new JPanel();
+        menu.setLayout(null);
+        mainmenu.setAndAdd(menu);
         mainmenu.add(menu);
-        //
         mainmenu.setResizable(false);
         mainmenu.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainmenu.setVisible(true);
         menu.setVisible(true);
+
     }
+    public void setUpButtonListeners() {
+        ActionListener bs = e -> {
+            Object click = e.getSource();
+            if(click == i) {
+                System.out.println("INFO");
+            }
+            if (click == easy){
+                System.out.println("easy");
+            }
+            if (click == medium){
+                System.out.println("medium");
+            }
+            if (click == hard){
+                System.out.println("hard");
+            }
+            if (click == leaderboard){
+                System.out.println("Leaderboard");
+            }
+        };
+        i.addActionListener(bs);
+        easy.addActionListener(bs);
+        medium.addActionListener(bs);
+        hard.addActionListener(bs);
+        leaderboard.addActionListener(bs);
+    }
+
 }
+
