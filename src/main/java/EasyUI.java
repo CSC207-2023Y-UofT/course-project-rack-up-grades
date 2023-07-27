6ebcaaef88d17f513384299e9f0f477ed2b9f6a5
Shreya Sakura Noskor <sakura.noskor12@gmail.com>
2023-07-27 19:20:25 -0400

    Easy UI works better now since we have the scores diplayed and as well as the buttons. The changes in file is just for demo and the changes in game use so that the file name is correct for my comupter

--- src/main/java/EasyUI.java
+++ src/main/java/EasyUI.java
@@ -89,6 +89,7 @@ public class EasyUI extends JPanel implements ActionListener {
                     but2.setVisible(false);
                     but3.setVisible(false);
                     but4.setVisible(false);
+                    title.setText("Score: "+viewM.getInfo().get(2));
                     String position = (viewM.getInfo().get(0)).substring(0,1);
                     switch (position){
                         case "1":
@@ -109,6 +110,10 @@ public class EasyUI extends JPanel implements ActionListener {
 
                 else {
                     T.cancel();
+                    but1.setVisible(false);
+                    but2.setVisible(false);
+                    but3.setVisible(false);
+                    but4.setVisible(false);
 
                 }
             }
